d537367d0e279f94fd24c9af35c936726f3648a4
Kai Hudalla <kai.hudalla@bosch-si.com>
2015-11-24 17:18:19 +0100

    Make PSK credentials configurable via command line or properties file.

--- scandium-server/src/main/java/org/eclipse/californium/scandium/server/Application.java
+++ scandium-server/src/main/java/org/eclipse/californium/scandium/server/Application.java
@@ -1,21 +1,19 @@
 package org.eclipse.californium.scandium.server;
 
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan
 @Configuration
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
 		SLF4JBridgeHandler.removeHandlersForRootLogger(); 
 		SLF4JBridgeHandler.install();
-//		LogManager.getLogManager().getLogger("").setLevel(Level.ALL);
 		SpringApplication.run(Application.class, args);
 	}
 
--- scandium-server/src/main/java/org/eclipse/californium/scandium/server/PskDtlsServer.java
+++ scandium-server/src/main/java/org/eclipse/californium/scandium/server/PskDtlsServer.java
@@ -17,6 +17,7 @@
 package org.eclipse.californium.scandium.server;
 
 import java.net.InetSocketAddress;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PreDestroy;
@@ -29,6 +30,7 @@ import org.eclipse.californium.scandium.config.DtlsConnectorConfig;
 import org.eclipse.californium.scandium.dtls.ConnectionStore;
 import org.eclipse.californium.scandium.dtls.InMemoryConnectionStore;
 import org.eclipse.californium.scandium.dtls.pskstore.InMemoryPskStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -36,19 +38,25 @@ import org.springframework.stereotype.Component;
 public class PskDtlsServer implements CommandLineRunner {
 
 	private static final Logger LOGGER = Logger.getLogger(PskDtlsServer.class.getName());
-	public static final int DEFAULT_PORT = 5684; 
-	public static final String CLIENT_IDENTITY = "Client_identity";
-	public static final String SECRET_KEY = "secretPSK";
 	private DTLSConnector dtlsConnector;
+
+	@Value(value="${scandium.client.identity}")
+	private String clientIdentity;
+
+	@Value(value="${scandium.client.secret}")
+	private String clientSecret;
+
+	@Value(value="${scandium.dtls.port}")
+	private int port;
 
 	public PskDtlsServer() {
 	}
 
 	@PreDestroy
 	public void destroy() throws Exception {
-		dtlsConnector.stop();
+		dtlsConnector.destroy();
 	}
-	
+
 	private class RawDataChannelImpl implements RawDataChannel {
 
 		private Connector connector;
@@ -70,10 +78,10 @@ public class PskDtlsServer implements CommandLineRunner {
 	public void run(String... arg0) throws Exception {
 
 		InMemoryPskStore pskStore = new InMemoryPskStore();
-		// put in the PSK store the default identity/psk for tinydtls tests
-		pskStore.setKey(CLIENT_IDENTITY, SECRET_KEY.getBytes());
+		LOGGER.log(Level.INFO, "Clients can authenticate to this server using PSK with identity [{0}]", clientIdentity);
+		pskStore.setKey(clientIdentity, clientSecret.getBytes());
 
-		DtlsConnectorConfig config = new DtlsConnectorConfig.Builder(new InetSocketAddress(DEFAULT_PORT))
+		DtlsConnectorConfig config = new DtlsConnectorConfig.Builder(new InetSocketAddress(port))
 			.setPskStore(pskStore)
 			.setMaxFragmentLengthCode(3)
 			.build();
